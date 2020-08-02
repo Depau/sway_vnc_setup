14342ca4e9e17f6df9e8f1b8a5b3a317b570d048
Davide Depau <davide@depau.eu>
2020-08-02 16:37:19 +0200

    Fix issues with WayVNC

--- sway_vnc_run.sh
+++ sway_vnc_run.sh
@@ -10,6 +10,15 @@ function outputs_options() {
   swaymsg -t get_outputs -r | jq -r '.[] | .name + "\n" + .make + " " + .model + " (" + (.current_mode.width | tostring) + "x" + (.current_mode.height | tostring) + ")"'
 }
 
+function options_options() {
+  echo disable
+  echo "Disable other displays"
+  echo 1
+  #echo dpms
+  #echo "Turn off selected local screen"
+  #echo 1
+}
+
 function listen_options() {
   ip -j address | jq -r '.[] | select(.flags | index("LOOPBACK") | not) | .ifname as $ifname | .addr_info[] | select(.scope != "link") | .local + "\n" + $ifname'
   echo '0.0.0.0'
@@ -20,19 +29,34 @@ function listen_options() {
 
 IFSBAK="$IFS"
 IFS=$'\n'
-output="$($dialog --stdout \
+output="$($dialog --args --stdout \
   --menu 'Which output you want to stream?' 0 0 0 $(outputs_options) \
-  --and-widget  --no-label 'Yes' --yes-label 'No' --yesno 'Do you want to turn off the other outputs?' 0 0)"
-turn_others_off=$?
-
-listen_addr="$($dialog --stdout \
-  --menu 'Address to listen on' 0 0 0 $(listen_options))"
+  --and-widget --no-tags --checklist 'Options' 0 0 0 $(options_options) \
+  --and-widget --menu 'Address to listen on' 0 0 0 $(listen_options))"
+IFS=$'\t'
+lines=($output)
 IFS="$IFSBAK"
 
-if [[ $turn_others_off == 1 ]]; then
-  swaymsg -t get_outputs -r | jq -r '.[].name' | while read -r out; do
-    swaymsg output "$out" disable
-  done
-fi
+output=${lines[0]}
+options=(${lines[1]})
+listen_addr=${lines[2]}
 
+clear
+
+for option in "${options[@]}"; do
+  echo "$option"
+  case $option in
+    disable)
+      swaymsg -t get_outputs -r | jq -r '.[].name | select(. != "'"$output"'")' | while read -r out; do
+        swaymsg output "$out" disable
+      done
+      ;;
+    dpms)
+      swaymsg output "$output" dpms off
+      ;;
+  esac
+done
+
+swaymsg output "$output" dpms on  # WayVNC crashes if the screen is off
 wayvnc -o "$output" "$listen_addr" & disown
+echo "WayVNC started, serving $output on $listen_addr"
--- sway_vnc_stop.sh
+++ sway_vnc_stop.sh
@@ -6,3 +6,7 @@ echo "Killing WayVNC"
 pkill wayvnc
 
 echo "Restoring outputs"
+swaymsg -t get_outputs -r | jq -r '.[].name | select(. | startswith("HEADLESS-") | not)' | while read -r output; do
+  swaymsg output "$output" enable
+  swaymsg output "$output" dpms on
+done
